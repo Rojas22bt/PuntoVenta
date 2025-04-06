410df6e4511eac41d0a19029143f7840d7926d4c
BoteritoROOT <d.sajor2003rp@gmail.com>
2025-04-06 16:49:37 -0400

    listo

--- src/componentes/Pages/Home.jsx
+++ src/componentes/Pages/Home.jsx
@@ -20,7 +20,7 @@ const Home = () => {
     <div className='home_fondo'>
       <div className='home_fondoG'>
         <div id="carouselExampleCaptions" className="carousel slide" data-bs-ride="carousel">
-          <div className="carousel-indicators">
+          <div className="carousel-indicators" id='indicador'>
             <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
             <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
             <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>

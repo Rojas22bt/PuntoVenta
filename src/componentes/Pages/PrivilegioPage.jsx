5be64414fc51cb5a53f7fcd3cc785acb7fe132cf
Joan Dr <joandanielrr@gmail.com>
2025-04-10 19:51:44 -0400

    asdf

--- src/componentes/Pages/PrivilegioPage.jsx
+++ src/componentes/Pages/PrivilegioPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import '../css/PrivilegioPage.css';
+import '../Css/PrivilegioPage.css'
 
 function PrivilegioPage() {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
